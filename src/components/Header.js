ab53ff102c6e776056f9d85cb7f3987e0c0bbc29
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-12-05 21:48:22 -0700

    Buttons and icon links added with responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 import test from '../data/images/test.jpg';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,26 +21,26 @@ const useStyles = makeStyles((theme) => ({
   },
 
   overlay: {
-    fontSize: '30pt',
     fontWeight: '900',
     position: 'absolute',
-    bottom: '10px',
+    bottom: '0px',
     left: '15px',
-    [theme.breakpoints.up('sm')]: {
+    // border: '2px solid red',
+    [theme.breakpoints.up('md')]: {
       fontSize: '44pt',
       left: '25px',
-      bottom: '60px'
+      bottom: '0px'
     },
     [theme.breakpoints.up('md')]: {
       fontSize: '56pt',
       left: '25px',
-      bottom: '60px'
+      bottom: '0px'
 
     }
   },
 
   name: {
-    fontSize: '30pt',
+    fontSize: '28pt',
     fontWeight: '900',
     [theme.breakpoints.up('sm')]: {
       fontSize: '44pt',
@@ -55,6 +60,56 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '9pt',
     fontWeight: '900',
     margin: '0rem 0.5rem 0rem 0rem'
+  },
+
+  buttonContainer: {
+    display: 'flex',
+    height: '35px',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
+  },
+
+  button: {
+    fontFamily: 'proxima-nova',
+    fontWeight: '400',
+    borderRadius: '20px',
+    margin: '0rem 1rem 0rem 0rem',
+    color: 'white',
+    borderColor: "#1db954",
+    transition: '0.2s ease-in-out',
+    '&:hover': {
+      background: '#1db954'
+    }
+  },
+
+  buttonText: {
+    fontSize: '9pt',
+  },
+
+  buttonIcon: {
+    
+  },
+
+  iconContainer: {
+    marginTop: '0.5rem',
+    height: '35px',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  },
+
+  icon: {
+    textDecoration: 'none',
+    margin: '0rem 2rem 0rem 0rem',
+    color: 'white',
+    transition: '0.2s ease-in-out',
+    '&:visited': {
+      color: 'white'
+    },
+    '&:hover': {
+      color: '#1db954'
+    }
   }
 
 }));
@@ -70,6 +125,31 @@ export default function Header() {
           <CheckCircleIcon style={{color: '#147efb'}}/>
         </div>
         <Typography className={classes.name}>Shadee Merhi</Typography>
+        <div className={classes.buttonContainer}>
+          <Button variant="outlined" className={classes.button} href="https://github.com/shadeemerhi">
+          <Typography className={classes.buttonText}>GitHub</Typography>
+          <GitHubIcon style={{marginLeft: '0.5rem'}}/>
+          </Button>
+          <Button variant="outlined" className={classes.button} href="https://www.linkedin.com/in/shadee-m-a27134ba/">
+          <Typography className={classes.buttonText}>LinkedIn</Typography>
+          <LinkedInIcon style={{marginLeft: '0.5rem'}}/>
+          </Button>
+          <Button variant="outlined" className={classes.button} href="mailto: shadmerhi@gmail.com">
+          <Typography className={classes.buttonText}>Email</Typography>
+          <EmailIcon style={{marginLeft: '0.5rem'}}/>
+          </Button>
+        </div>
+        <div className={classes.iconContainer}>
+          <Link href="https://github.com/shadeemerhi" className={classes.icon}>
+            <GitHubIcon/>
+          </Link>
+          <Link href="https://www.linkedin.com/in/shadee-m-a27134ba/" className={classes.icon}>
+            <LinkedInIcon/>
+          </Link>
+          <Link href="mailto: shadmerhi@gmail.com" className={classes.icon}>
+            <EmailIcon/>
+          </Link>
+        </div>
       </div>
     </div>
   )
