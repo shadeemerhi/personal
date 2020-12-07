979b9c20050ada5df45f827478237fcc85dd7ac6
Shadee Merhi <shadmerhi@gmail.com>
2020-12-06 21:22:37 -0700

    initialized data files

---
+++ src/data/work.js
@@ -0,0 +1,15 @@
+export const workExperiences = [
+  {
+    id: 1,
+    company: 'HiOctave Media',
+    title: 'Founder and Creative Director',
+    date: 'Feb 2020 - Present',
+    location: 'Calgary, AB',
+    items: [
+      'Provide custom website development and digital marketing services to small businesses',
+      'Grew team to 3 employees that currently service 5 clients on an on-going basis',
+      'Create marketing content to display on client websites and their social media platforms',
+      'Content creation includes: graphic design, photography, and videography',
+    ]
+  },
+]
