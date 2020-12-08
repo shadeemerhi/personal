ecc760c147910011441e0afbcb10bb011043a8de
Shadee Merhi <shadmerhi@gmail.com>
2020-12-07 17:56:56 -0700

    coursera courses added

--- src/data/education.js
+++ src/data/education.js
@@ -2,7 +2,7 @@ export const educationItems = [
   {
     id: 1,
     school: 'Lighthouse Labs',
-    program: 'Full-stack Web Development',
+    program: 'Full Stack Web Development',
     date: 'Sep 2020 - Dec 2020',
     image: 'lighthouse.png'
   },
@@ -13,4 +13,32 @@ export const educationItems = [
     date: '2016 - 2020',
     image: 'ucalgary.png'
   },
+  {
+    id: 3,
+    school: 'Coursera',
+    program: 'Object Oriented Programming in Java',
+    date: 'May 2020',
+    image: 'coursera.png'
+  },
+  {
+    id: 4,
+    school: 'Coursera',
+    program: 'Java: Principles of Software Design',
+    date: 'Apr 2020',
+    image: 'coursera.png'
+  },
+  {
+    id: 5,
+    school: 'Coursera',
+    program: 'Java: Arrays, Lists, and Structured Data',
+    date: 'Mar 2020',
+    image: 'coursera.png'
+  },
+  {
+    id: 6,
+    school: 'Coursera',
+    program: 'Java: Solving Problems with Software',
+    date: 'Mar 2020',
+    image: 'coursera.png'
+  }
 ]
