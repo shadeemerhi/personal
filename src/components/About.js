4af8ac27938b0d6b6186c78c0e79e25e3c3cdad4
Shadee Merhi <shadmerhi@gmail.com>
2020-12-07 19:58:51 -0700

    latest project showing - still needs added state

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { projects } from '../data/projects';
+import Project from './Project';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+  }
+}));
+
+
 export default function About(props) {
+
+  const latestProject = projects[0];
+
+
+  const classes = useStyles();
 
   return(
     <div>
+      <Project {...latestProject}/>
       <p>{props.textMain}</p>
       <br/>
       <p>{props.textSub}</p>
