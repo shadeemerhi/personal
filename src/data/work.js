cb27b337e24224900954dc1f4e8a0c38eeae1499
Shadee Merhi <shadmerhi@gmail.com>
2020-12-09 20:20:55 -0700

    updated work information

--- src/data/work.js
+++ src/data/work.js
@@ -21,7 +21,7 @@ export const workExperiences = [
     location: 'Calgary, AB',
     items: [
       "Assisted senior engineers in analyzing mechanical diagrams such as P&ID's, ISO's and General Assemblies to create inspection plans for static and rotating equipment and facility pipelines",
-      'Developed 50+ asset inspection profiles on Meridium',
+      'Developed 50+ asset inspection profiles on Meridium which were estimated to reduce maintenance costs by $200,000',
       'Collaborated with on-site inspectors to develop an inspection reporting system',
       'Developed site plot plan for the 2020 turnaround work'
     ]
