e476824ea1bfb7e100d41c94e9070c2d06a24791
Shadee Merhi <shadmerhi@gmail.com>
2021-09-19 22:07:38 -0600

    add DEFAULT_WORKITEM, refactor DEFAULT_PROJECT

--- src/data/default_objects.js
+++ src/data/default_objects.js
@@ -1,18 +1,23 @@
 export const DEFAULT_PROJECT = {
-    projects: [],
-    latestProject: {
-        id: null,
-        name: null,
-        description: '',
-        image: '',
-        stack: {
-            front: [],
-            back: [],
-            deployment: [],
-            test: [],
-            other: []
-        },
-        date: '',
-        url: ''
-    }
+    id: null,
+    name: null,
+    description: '',
+    image: '',
+    stack: {
+        front: [],
+        back: [],
+        deployment: [],
+        test: [],
+        other: []
+    },
+    date: '',
+    url: ''
 }
+
+export const DEFAULT_WORKITEM = {
+	company: '',
+	title: '',
+	date: '',
+	location: '',
+	items: []
+};
