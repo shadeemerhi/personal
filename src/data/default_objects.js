fae10931aa4da84dcb1da9ce054ee23cea0bec3b
Shadee Merhi <shadmerhi@gmail.com>
2021-09-19 20:33:13 -0600

    create default objects file

---
+++ src/data/default_objects.js
@@ -0,0 +1,18 @@
+export const DEFAULT_PROJECT = {
+    projects: [],
+    latestProject: {
+        id: null,
+        name: null,
+        description: '',
+        image: '',
+        stack: {
+            front: [],
+            back: [],
+            deployment: [],
+            test: [],
+            other: []
+        },
+        date: '',
+        url: ''
+    }
+}
